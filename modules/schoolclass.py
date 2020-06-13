62f9649a34b4d65e5a9cdeee36f200a042a11707
fatsoflo <fatsoflo@users.noreply.github.com>
2020-06-13 00:45:54 -0400

    update user_directory_path
    probably temporary until we figure out file submissions more

--- modules/schoolclass.py
+++ modules/schoolclass.py
@@ -10,7 +10,7 @@ class SchoolClass(models.Model):
 # use to specify directory path
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(user.id, filename)
+    return 'user_{0}/{1}'.format(instance.owner.id, filename)
 
 # Project is made up of many Missions
 class Mission(models.Model):
