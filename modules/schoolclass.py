e4d0c5d49f618634ccb40a796e13d54879894955
fatsoflo <fatsoflo@users.noreply.github.com>
2020-06-12 23:40:13 -0400

    Update schoolclass.py

--- modules/schoolclass.py
+++ modules/schoolclass.py
@@ -1,28 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class SchoolClass(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return f'{self.name}'
-
-    
-class Project(models.Model):
-    STATUS_CHOICES = [
-        ('A', 'Assigned'),
-        ('S', 'Submitted'),
-        ('O', 'Overdue')
-    ]    
-
-    status = models.CharField(
-        max_length=1,
-        choices=STATUS_CHOICES,
-        default="A"
-    ) 
-
-    submission = models.FileField(upload_to="", null=True, blank=True)
-
-    missions = models.ManyToManyField(Mission, null=True)
 
 # project is made up of mission components
 class Mission(models.Model):
@@ -37,6 +20,31 @@ class Mission(models.Model):
         default="A"
     )   
 
-    submission = models.FileField(upload_to="", null=True, blank=True)
+    submission = models.FileField(upload_to="temp", null=True, blank=True)
 
     reward = models.IntegerField(default=0)
+
+    
+
+
+class Project(models.Model):
+    STATUS_CHOICES = [
+        ('A', 'Assigned'),
+        ('S', 'Submitted'),
+        ('O', 'Overdue'),
+        ('SL', 'Submitted Late')
+    ]    
+
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default="A"
+    ) 
+
+    submission = models.FileField(upload_to="", null=True, blank=True)
+
+    missions = models.ManyToManyField(Mission, null=True)
+
+    date_assign = models.DateTimeField(default=timezone.now)
+
+    date_due = models.DateTimeField(default=timezone.now)
